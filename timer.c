90e744a078b49bed34a936268e3e5488364df2a2
Daniel Engel <dengeltheyounger@protonmail.com>
2021-08-04 16:27:43 -0400

    testing change

--- timer.c
+++ timer.c
@@ -4,12 +4,10 @@ volatile struct timer system_time;
 
 uint8_t set_timer() {
 
-	TCCR2B = 0;
-	TCCR2A = 0;
-	TCCR2A |= (1 << WGM21);
-	OCR2A = 249;
+	TCCR2A = (1 << WGM21);
+	TCCR2B = (1 << CS22);
 	TCNT2 = 0;
-	TCCR2B |= (1 << CS22);
+	OCR2A = 249;
 	
 	// Interrupt every ms
 	TIMSK2 |= (1 << OCIE2A);
