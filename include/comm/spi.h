644ea2ec5bebf276feed5258967dfa6c8c93ca86
dengeltheyounger <dengeltheyounger@protonmail.com>
2022-06-25 14:39:04 -0400

    More additions to spi

--- include/comm/spi.h
+++ include/comm/spi.h
@@ -9,11 +9,15 @@
 #define	SPI_MOSI	PB3
 #define	SPI_SS		PB2
 
+#define	SPI_SS_LOW()	PORTB &= ~(1 << SPI_SS)
+#define	SPI_SS_HIGH()	PORTB |= (1 << SPI_SS)
+
 void spi_init();
 
 void spi_send( uint8_t *buffer, uint16_t bytes );
 
 void spi_receive( uint8_t *buffer, uint16_t bytes );
 
+#endif
 
 #endif
--- src/comm/spi.c
+++ src/comm/spi.c
@@ -9,9 +9,17 @@
  *	the mode is set to 0.
  */
 void spi_init() {
+	uint8_t clear;
 	DDR_SPI = (1 << SPI_MOSI) | (1 << SPI_SCK) | (1 << SPI_SS);
 
-	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0) | (1 << SPIE);
+	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+
+	SPI_SS_LOW();
+
+	SPI_SS_HIGH();
+
+	clear = SPSR;
+	clear = SPDR;
 }
 
 /*!
@@ -20,10 +28,12 @@ void spi_init() {
  *	\param byte
  *	The byte to send.
  */
-void spi_send_byte(uint8_t byte) {
+uint8_t do_spi(uint8_t byte) {
 	SPDR = byte;
 
 	while (!(SPSR & (1 << SPIF));
+
+	return SPDR;
 }
 
 /*!
@@ -37,9 +47,25 @@ void spi_send_byte(uint8_t byte) {
  *
  */
 void spi_send(uint8_t *buffer, uint16_t bytes) {
+	
+	SPI_SS_LOW();
 	for (uint16_t i = 0; i < bytes; ++i) {
-		spi_send_byte(&buffer[i]);
+		do_spi(&buffer[i]);
 	}
+
+	SPI_SS_HIGH();
 }
 
+void spi_receive(uint8_t *buffer, uint16_t bytes) {
+	uint8_t byte;
 
+	SPI_SS_LOW();
+
+	for (uint16_t i = 0; i < bytes; ++i) {
+		byte = do_spi(0xff);
+	}
+
+	SPI_SS_HIGH();
+}
+
+#endif
